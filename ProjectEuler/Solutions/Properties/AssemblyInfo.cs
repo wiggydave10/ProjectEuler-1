b4387e86f16546bfc3d4cbed9dd7713fec939e2a
b-hub <ben.magistris@gmail.com>
2017-03-18 19:03:29 +0000

    restructuring

--- ProjectEuler/Solutions/Properties/AssemblyInfo.cs
+++ ProjectEuler/Solutions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Problem1")]
+[assembly: AssemblyTitle("Solution")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Problem1")]
+[assembly: AssemblyProduct("Solution")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
