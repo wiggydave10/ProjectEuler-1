cc0ec189bd2f63e43e6c7303b8269182a423041f
b-hub <ben.magistris@gmail.com>
2017-03-18 11:02:26 +0000

    Problem 3 - Largest prime factor

---
+++ ProjectEuler/Problem1/Problem3/V1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problem3
+{
+    public static class V1
+    {
+        public static int LargestPrimeFactor(long n)
+        {
+            return PrimeFactors(n).Max();
+        }
+
+        private static IEnumerable<int> PrimeFactors(long n)
+        {
+            foreach (var prime in Primes())
+            {
+                if (n == 1) yield break;
+                if (n % prime != 0) continue;
+
+                n = n / prime;
+                yield return prime;
+            }
+        }
+
+        private static IEnumerable<int> Primes()
+        {
+            int curr = 1;
+            while (true)
+            {
+                curr++;
+                if (IsPrime(curr)) yield return curr;
+            }
+        }
+
+        private static bool IsPrime(int x)
+        {
+            return Enumerable.Range(2, x - 2).All(i => x%i != 0);
+        }
+    }
+}
---
+++ ProjectEuler/UnitTests/Problem3Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problem3;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Problem3Tests
+    {
+        [TestMethod]
+        public void Version1Tests()
+        {
+            Assert.AreEqual(5, V1.LargestPrimeFactor(10));
+            Assert.AreEqual(29, V1.LargestPrimeFactor(13195));
+            Assert.AreEqual(6857, V1.LargestPrimeFactor(600851475143));
+        }
+    }
+}
