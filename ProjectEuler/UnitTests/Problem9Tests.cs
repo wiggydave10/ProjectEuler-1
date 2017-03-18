31d90937997c76444420c1ee9f45f3d567a0b466
b-hub <ben.magistris@gmail.com>
2017-03-18 18:06:45 +0000

    Problem 9

---
+++ ProjectEuler/UnitTests/Problem9Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problem9;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Problem9Tests
+    {
+        [TestMethod]
+        public void Version1Tests()
+        {
+            Assert.AreEqual(31875000, V1.Answer());
+        }
+    }
+}
