ec10a5952e8466d965508c7518ffd6da28e470d5
b-hub <ben.magistris@gmail.com>
2017-03-18 10:26:10 +0000

    Problems 1 & 2

---
+++ ProjectEuler/Problem1/Problem1/V1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Problem1
+{
+    public static class V1
+    {
+        public static long SumMultiples(int n)
+        {
+            return GetMultiples(n).Sum();
+        }
+
+        private static IEnumerable<long> GetMultiples(int n)
+        {
+            for (var i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) yield return i;
+            }
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEuler/V1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public static class V1
+    {
+        public static int SumMultiples(int n)
+        {
+            return GetMultiples(n).Sum();
+        }
+
+        private static IEnumerable<int> GetMultiples(int n)
+        {
+            for (var i = 1; i < n; i++)
+            {
+                if (i%3 == 0 || i%5 == 0) yield return i;
+            }
+        }
+    }
+}
