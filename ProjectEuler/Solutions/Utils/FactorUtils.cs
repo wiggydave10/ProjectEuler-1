8b64275b9f913855db4b6f6b3afab8eead8722ad
b-hub <ben.magistris@gmail.com>
2017-03-19 13:56:01 +0000

    Problem 12 + new factor utils

---
+++ ProjectEuler/Solutions/Utils/FactorUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Utils
+{
+    public static class FactorUtils
+    {
+        public static IEnumerable<int> GetFactors(int n)
+        {
+            return Enumerable.Range(1, n/2).Where(i => n%i == 0);
+        }
+
+        public static IEnumerable<long> GetFactors(long n)
+        {
+            var lowestOtherFactor = n;
+            for (var i = 1; i < lowestOtherFactor; i++)
+            {
+                if (n % i != 0) continue;
+                var otherFactor = n/i;
+                yield return i;
+                yield return otherFactor;
+                lowestOtherFactor = Math.Min(lowestOtherFactor, otherFactor);
+            }
+
+            if (n == 1) yield return 1;
+        }
+    }
+}
---
+++ ProjectEuler/UnitTests/UtilityTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problem012;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void FactorUtils_GetFactors_1()
+        {
+            AssertEqual(new long[] {1}, FactorUtils.GetFactors((long)1));
+        }
+
+        [TestMethod]
+        public void FactorUtils_GetFactors_2()
+        {
+            AssertEqual(new long[] { 1,2 }, FactorUtils.GetFactors((long)2));
+        }
+
+        [TestMethod]
+        public void FactorUtils_GetFactors_10()
+        {
+            AssertEqual(new long[] { 1,2,5,10 }, FactorUtils.GetFactors((long)10));
+        }
+
+        [TestMethod]
+        public void FactorUtils_GetFactors_28()
+        {
+            AssertEqual(new long[] { 1, 2, 4, 7, 14, 28 }, FactorUtils.GetFactors((long)28));
+        }
+
+        private void AssertEqual<T>(IEnumerable<T> xs, IEnumerable<T> ys)
+        {
+            var xsArr = xs.OrderBy(x => x).ToArray();
+            var ysArr = ys.OrderBy(y => y).ToArray();
+            Assert.AreEqual(xsArr.Length, ysArr.Length);
+            Assert.IsTrue(xsArr.Select((x,i) => x.Equals(ysArr[i])).All(b => b));
+        }
+    }
+}
