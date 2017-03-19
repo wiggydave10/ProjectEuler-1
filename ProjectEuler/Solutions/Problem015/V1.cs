86e3d9cd05980a78e7ba5572db673fb17e2f6dc3
b-hub <ben.magistris@gmail.com>
2017-03-19 17:31:57 +0000

    Problem 15 v1 & 2

---
+++ ProjectEuler/Solutions/Problem015/V1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problem015
+{
+    public static class V1
+    {
+        public static ulong LatticePathCount(int n)
+        {
+            var row = new List<ulong> {1};
+
+            for (var i = 1; i < n+1; i++)
+            {
+                ulong prev = 1;
+                for (var j = 1; j < i; j++)
+                {
+                    row[j] += prev;
+                    prev = row[j];
+                }
+                row.Add(prev * 2);
+            }
+
+            return row.Last();
+        }
+    }
+}
