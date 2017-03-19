c6e38869f346d8ac6541684c36dc3d41fab81400
b-hub <ben.magistris@gmail.com>
2017-03-19 15:56:10 +0000

    Problems 13 + 14

---
+++ ProjectEuler/Solutions/Problem013/V1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problem013
+{
+    public static class V1
+    {
+        public static IEnumerable<int> FirstNDigitsOfLargeSum(int n, int[][] xs)
+        {
+            return xs.Aggregate(Add).Take(n);
+        }
+
+        private static int[] Add(int[] a, int[] b)
+        {
+            var rSmaller = (a.Length < b.Length) ? a.Reverse().ToArray() : b.Reverse().ToArray();
+            var rBigger = (a.Length < b.Length) ? b.Reverse().ToArray() : a.Reverse().ToArray();
+
+            var res = new List<int>();
+            var carry = 0;
+            for (var i = 0; i < rSmaller.Length; i++)
+            {
+                var sum = rSmaller[i] + rBigger[i] + carry;
+                
+                res.Add(sum % 10);
+                carry = sum >= 10 ? 1 : 0;
+            }
+
+            foreach (var x in rBigger.Skip(rSmaller.Length))
+            {
+                var sum = x + carry;
+                res.Add(sum % 10);
+                carry = sum >= 10 ? 1 : 0;
+            }
+
+            if (carry == 1) res.Add(1);
+
+            res.Reverse();
+            return res.ToArray();
+        }
+    }
+}
---
+++ ProjectEuler/UnitTests/ProblemTests013.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problem013;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProblemTests013
+    {
+        private int[][] numbers;
+
+        public ProblemTests013()
+        {
+            numbers =
+                File.ReadAllLines("../../Resources/Problem013.txt")
+                    .Select(l => l.Select(ch => ch.ToString()).Select(int.Parse).ToArray())
+                    .ToArray();
+        }
+
+        [TestMethod]
+        public void Version1_FirstNDigitsOfLargeSum()
+        {
+            Assert.AreEqual("5537376230", string.Join("", V1.FirstNDigitsOfLargeSum(10, numbers)));
+        }
+    }
+}
