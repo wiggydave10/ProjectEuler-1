9f5f15b22b23775b894157d8e895a0b26c5bd2e4
b-hub <ben.magistris@gmail.com>
2017-03-20 21:01:13 +0000

    Problems 18 & 19

---
+++ ProjectEuler/UnitTests/ProblemTests019.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions.Problem019;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProblemTests019
+    {
+        [TestMethod]
+        public void Version1_CountingSundays_local()
+        {
+            Assert.AreEqual(0, V1.CountingSundays(DateTime.Parse("04/03/2017"), DateTime.Parse("20/03/2017")));
+        }
+
+        [TestMethod]
+        public void Version1_CountingFirstSundays_local()
+        {
+            Assert.AreEqual(2, V1.CountingFirstSundays(DateTime.Parse("01/01/2017"), DateTime.Parse("31/12/2017")));
+        }
+
+        [TestMethod]
+        public void Version1_CountingSundays_Answer()
+        {
+            Assert.AreEqual(171, V1.CountingFirstSundays(DateTime.Parse("01/01/1901"), DateTime.Parse("31/12/2000")));
+        }
+    }
+}
