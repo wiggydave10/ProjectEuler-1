2d9b8b41b68ba99ae9fc80aedabce1d441699996
b-hub <ben.magistris@gmail.com>
2017-03-26 20:45:09 +0100

    refactored into mathematicalObjects namespace

---
+++ ProjectEuler/Solutions/Utils/MathematicalObjects/Constant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Utils.MathematicalObjects
+{
+    public class Constant : Number
+    {
+        public Constant()
+        {
+            Value = 0;
+        }
+
+        public Constant(double value)
+        {
+            Value = value;
+        }
+
+        public double Value { get; set; }
+
+        public override Number Clone()
+        {
+            return new Constant(Value);
+        }
+
+        public override Number Minus()
+        {
+            Value = -Value;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString("F");
+        }
+
+        public static Constant operator +(Constant c1, Constant c2)
+        {
+            return new Constant(c1.Value + c2.Value);
+        }
+    }
+}
---
+++ ProjectEuler/Solutions/Utils/MathematicalObjects/Number.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Utils.MathematicalObjects
+{
+    public abstract class Number
+    {
+        public abstract Number Clone();
+        public abstract Number Minus();
+
+        public static Number operator /(Number v1, double x)
+        {
+            var clone = v1.Clone();
+            if (v1.GetType() == typeof(Variable))
+            {
+                var cloneV = (Variable)clone;
+                cloneV.Coefficient /= x;
+                return cloneV;
+
+            }
+
+            var cloneC = (Constant)clone;
+            cloneC.Value /= x;
+            return cloneC;
+        }
+
+        public static Number operator *(Number v1, double x)
+        {
+            var clone = v1.Clone();
+            if (v1.GetType() == typeof(Variable))
+            {
+                var cloneV = (Variable)clone;
+                cloneV.Coefficient *= x;
+                return cloneV;
+
+            }
+
+            var cloneC = (Constant)clone;
+            cloneC.Value *= x;
+            return cloneC;
+        }
+    }
+}
