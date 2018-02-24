a723a3e1ac47203b66a0450e0b0a464553c83864
b-hub <ben.magistris@gmail.com>
2018-02-24 19:58:50 +0000

    solved P033 - requires code to reduce fraction to simplest form

---
+++ ProjectEuler/UnitTests/P033DigitCancellingFractions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using Solutions.Problem033;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class P033DigitCancellingFractions
+    {
+        [TestMethod]
+        public void Version1_Get()
+        {
+            var f = V1.Get().Aggregate((a, b) => a * b); // simplest 1/100
+        }
+    }
+}
