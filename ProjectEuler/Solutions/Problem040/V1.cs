6942d2693c2c66637589347386f9f8cafa6e239d
b-hub <ben.magistris@gmail.com>
2018-02-25 11:31:53 +0000

    P040 solved

---
+++ ProjectEuler/Solutions/Problem040/V1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Solutions.Utils;
+
+namespace Solutions.Problem040
+{
+    /*
+        Champernowne's constant
+        Problem 40 
+        An irrational decimal fraction is created by concatenating the positive integers:
+
+        0.123456789101112131415161718192021...
+
+        It can be seen that the 12th digit of the fractional part is 1.
+
+        If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+        d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+    */
+    public static class V1
+    {
+        public static int Get()
+        {
+            var fractionDigits = ChampernownesSequence(1000000).ToArray();
+            var fractionalParts = Enumerable.Range(0, 7).Select(x => (int) Math.Pow(10, x) - 1).ToArray();
+
+            return fractionalParts.Product(i => fractionDigits[i]);
+        }
+
+        public static IEnumerable<int> ChampernownesSequence(int limit)
+        {
+            return Enumerable.Range(1, limit).SelectMany(i => MiscUtils.GetDigits(i).Reverse());
+        }
+    }
+}
---
+++ ProjectEuler/UnitTests/P039IntegerRightTriangles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using Solutions.Problem039;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class P039IntegerRightTriangles
+    {
+        [TestMethod]
+        public void Version1_Practise()
+        {
+            var solutions = V1.GetSolutions(120).ToArray();
+            V1.GetSolutions(120).Count().ShouldBe(3);
+        }
+
+        [TestMethod]
+        public void Version1_Answer()
+        {
+            V1.PerimeterWithMaxSolutions(1000).ShouldBe(840);
+        }
+    }
+}
