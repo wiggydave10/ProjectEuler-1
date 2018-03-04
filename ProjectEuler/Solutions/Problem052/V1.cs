a8723e3b264fca35c506e5bcf3588077802348c2
b-hub <ben.magistris@gmail.com>
2018-03-04 11:37:11 +0000

    P052

---
+++ ProjectEuler/Solutions/Problem052/V1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Solutions.Utils;
+
+namespace Solutions.Problem052
+{
+    /*
+        Permuted multiples
+        Problem 52 
+        It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+        Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+    */
+
+    public static class V1
+    {
+        public static long Main(int n)
+        {
+            var curr = 2;
+            var currHash = PermutatedHash(curr);
+
+            while (Enumerable.Range(2, n - 2).Select(i => PermutatedHash(curr * i)).Any(x => x != currHash))
+            {
+                currHash = PermutatedHash(++curr);
+            }
+
+            return curr;
+        }
+
+        public static string PermutatedHash(int n)
+        {
+            return new string(n.ToString().OrderBy(x => x).ToArray());
+        }
+    }
+}
