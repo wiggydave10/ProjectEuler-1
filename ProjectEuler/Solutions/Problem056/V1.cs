f7aa7fb950f1c6ddc21b7fc27c61ef58581fd5fb
b-hub <ben.magistris@gmail.com>
2018-03-09 22:29:19 +0000

    p056

---
+++ ProjectEuler/Solutions/Problem056/V1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Solutions.Utils;
+
+namespace Solutions.Problem056
+{
+    /*
+        Powerful digit sum
+        Problem 56 
+        A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+        Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum?
+    */
+
+    public static class V1
+    {
+        public static int MaximalDigitSum(int limit)
+        {
+            var maxDigitSum = 0;
+            for (var i = new BigInteger(1); i < limit; i++)
+            {
+                for (var j = 1; j < limit; j++)
+                {
+                    var digitSum = BigInteger.Pow(i, j).GetDigits().Sum();
+                    maxDigitSum = Math.Max(maxDigitSum, digitSum);
+
+                    if (digitSum == maxDigitSum)
+                    {
+                        
+                    }
+                }
+            }
+
+            return maxDigitSum;
+        }
+    }
+}
