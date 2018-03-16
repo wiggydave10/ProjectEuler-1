86346af2395d1d821923566f4fa282291bd05b8c
b-hub <ben.magistris@gmail.com>
2018-03-16 20:02:42 +0000

    move solutions into Level folders

---
+++ ProjectEuler/UnitTests/P059XORDecryption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using Solutions.Problem059;
+using Solutions.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class P059XORDecryption
+    {
+        [TestMethod]
+        public void Version1_Practise()
+        {
+        }
+    }
+}
